31df621733e578e7e960c67ee93129180dd16bfe
追马 <wng_xng@126.com>
2026-01-04 20:59:39 +0800

    add chapter 15

---
+++ chapter-15-document/docstrings.py
@@ -0,0 +1,25 @@
+
+"""
+Module document
+Words Go Here
+"""
+spam = 40
+
+def square(x):
+    """
+    fucntion document
+    can we have your liver then?
+    """
+    return x ** 2
+
+class Employee:
+    """
+    class documentation
+    """
+    pass
+    
+    def f1():
+        """
+        类的方法的document
+        """
+        pass
